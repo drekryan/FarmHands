e5528d7414a86a10c244a9c82fd6ed8570ca4a39
Derrick Lambert <drekryan@yahoo.com>
2016-10-23 00:49:28 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+FarmHands
+=========
+
+Author: [Drekryan](https://www.github.com/Drekryan)
+Requires: WorldGuard 6.1.3-SNAPSHOT
+
+FarmHands is a Minecraft Spigot Plugin that adds a custom WorldGuard flag
+to auto replace crops when they are harvested.
+
+This allows players to harvest crops without having to remember to
+replant it. That way your farms are always fully planted and ready
+to grow.
---
+++ src/main/java/com/Drekryan/FarmHands/FarmHands.java
@@ -0,0 +1,43 @@
+package com.Drekryan.FarmHands;
+
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.BooleanFlag;
+import com.sk89q.worldguard.protection.flags.Flag;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FarmHands extends JavaPlugin {
+    private WorldGuardPlugin worldGuard;
+    private static Flag REPLACE_CROPS = new BooleanFlag("replace-crops");
+
+    @Override
+    public void onLoad() {
+        this.worldGuard = getWorldGuard();
+
+        if (worldGuard != null) {
+            worldGuard.getFlagRegistry().register(REPLACE_CROPS);
+            this.getLogger().info("Custom WorldGuard Flag has been registered");
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Successfully Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info("Disabled FarmHands...");
+    }
+
+    private WorldGuardPlugin getWorldGuard() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
+
+        // WorldGuard may not be loaded
+        if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
+            return null;
+        }
+
+        return (WorldGuardPlugin) plugin;
+    }
+}
